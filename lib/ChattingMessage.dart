882f9f0e14f08eecaaae5b940b46369ef30d27e8
Yeob <happyk100@naver.com>
2021-08-16 17:08:04 +0900

    first version - text한정 create, read 가능

--- lib/ChattingMessage.dart
+++ lib/ChattingMessage.dart
@@ -1,18 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 // DateFormat time 하는거
 import 'package:intl/intl.dart';
 
-class Message extends StatelessWidget {
-  final String sender = '100';
-  final String text;
-  final DateTime time;
 
-  Message(this.text, this.time);
+
+class Message extends StatelessWidget {
+  final String sender;
+  final String text;
+  final Timestamp stamp;
+  // final DateTime time;
+
+  Message(this.sender, this.text, this.stamp);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+    if (sender == '200') return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
       child: Row(
         children: [
           //프로필 사진
@@ -27,21 +31,63 @@ class Message extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(sender, style: TextStyle(fontWeight: FontWeight.bold)),
-                Text(text),
+                Row(
+                  children: [
+                    Container(
+                      child: Text(text, style: TextStyle(fontSize: 15)),
+                      padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+                      decoration: BoxDecoration(color: const Color(0x9ebed8ff), borderRadius: BorderRadius.circular(8.0)),
+                    ),
+                    SizedBox(width: 5),
+                    Column(
+                      children: [
+                        SizedBox(height: 13),
+                        Text(
+                          DateFormat('kk:mma').format(stamp.toDate()).toString(),
+                          style: TextStyle(color: Colors.grey, fontSize: 13.5),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
-          // 시간 표시
+          // Column(
+          //     crossAxisAlignment: CrossAxisAlignment.start,
+          //     children: [
+          //         Text(
+          //           DateFormat('kk:mma').format(stamp.toDate()).toString(),
+          //           // DateFormat('kk:mma').format(time).toString(),
+          //           // 연월일 표시 : DateFormat('yyyy-MM-dd, kk:mma').format(date).toString(),
+          //           style: TextStyle(color: Colors.grey),
+          //         ),
+          //     ],
+          //   ),
+        ],
+      ),
+    );
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
           Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
+              SizedBox(height: 13),
               Text(
-                //toString 꼭 해야하는가 ?
-                DateFormat('kk:mma').format(time).toString(),
+                DateFormat('kk:mma').format(stamp.toDate()).toString(),
+                style: TextStyle(color: Colors.grey, fontSize: 13.5),
+                // DateFormat('kk:mma').format(time).toString(),
                 // 연월일 표시 : DateFormat('yyyy-MM-dd, kk:mma').format(date).toString(),
-                style: TextStyle(color: Colors.grey),
               ),
             ],
+          ),
+          SizedBox(width: 5),
+          Container(
+            child: Text(text, style: TextStyle(fontSize: 15)),
+            padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+            decoration: BoxDecoration(color: const Color(0xf7eeeeee), borderRadius: BorderRadius.circular(8.0)),
           ),
         ],
       ),
--- lib/ChattingRoom.dart
+++ lib/ChattingRoom.dart
@@ -2,6 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'ChattingMessage.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 class ChatRoom extends StatefulWidget {
   @override
@@ -10,116 +13,107 @@ class ChatRoom extends StatefulWidget {
 
 class _ChatRoomState extends State<ChatRoom> {
   TextEditingController _textEditingController = TextEditingController();
-  // ScrollController _scollController = ScrollController();
-
-  CollectionReference col = FirebaseFirestore.instance.collection('Chatroom');
-
-  //message를 list에 담아둠.
-  List<Message> _chats = [];
+  ScrollController _scollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
-    var documents;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         title: Text('user'),
       ),
-      body: Column(
-        children: [
-          //message list 가져오기
-          messageList(),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: Row(
-              children: [
-                //하단바
-                Expanded(
-                  // 메세지 입력창
-                  child: TextField(
-                    controller: _textEditingController,
-                    decoration: InputDecoration(hintText: '메세지 입력'),
-                    // enter로 제출 시
-                    onSubmitted: (input) {
-                      _handleSubmitted();
-                    },
+      body: SafeArea(
+        child: Column(
+          children: [
+            //message list 가져오기
+            messageList(),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: Row(
+                children: [
+                  //하단바
+                  Expanded(
+                    // 메세지 입력창
+                    child: TextField(
+                      controller: _textEditingController,
+                      decoration: InputDecoration(hintText: '메세지 입력'),
+                      // enter로 제출 시
+                      onSubmitted: (input) {
+                        _handleSubmitted('100', _textEditingController.text, Timestamp.now());
+                      },
+                    ),
                   ),
-                ),
-                SizedBox(
-                  // 입력창, 버튼 거리 띄우기
-                  width: 10.0,
-                ),
-                // 보내기 버튼
-                MaterialButton(
-                  onPressed: () {
-                    _handleSubmitted(
-                        //documents.single.get('writer')
-                    );
-                  },
-                  child: Text('send'),
-                  color: const Color(0xff6990FF),
-                ),
-              ],
+                  SizedBox(
+                    // 입력창, 버튼 거리 띄우기
+                    width: 10.0,
+                  ),
+                  // 보내기 버튼
+                  MaterialButton(
+                    onPressed: () {_handleSubmitted('100', _textEditingController.text, Timestamp.now());},
+                    child: Text('send'),
+                    color: const Color(0xff6990FF),
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 25.0,
-          )
-        ],
+            // SizedBox(
+            //   height: 25.0,
+            // )
+          ],
+        ),
       ),
     );
   }
 
-  void _handleSubmitted() {
-    if (_textEditingController.text.length > 0) {
-      final newMessage = Message(_textEditingController.text, DateTime.now());
-
-
-      setState(() {
-        _chats.insert(0, newMessage);
-      });
+  void _handleSubmitted(String sender, String text, Timestamp stamp) {
+    if (text.trim() != '') {
       _textEditingController.clear();
-      // _animListKey.currentState!.insertItem(0);
+
+      var documentReference = FirebaseFirestore.instance
+          .collection('Chatroom/room1/messages')
+          .doc();
+
+      FirebaseFirestore.instance.runTransaction((transaction) async => {
+        transaction.set(
+          documentReference,
+          {
+            'sender': sender,
+            'text': text,
+            'time': stamp,
+          },
+        )
+      });
+
+      // final newMessage = Message(_textEditingController.text, DateTime.now().toString());
+    }
+    else {
+      Fluttertoast.showToast(msg: 'Nothing to send', backgroundColor: Colors.black, textColor: Colors.red);
     }
   }
 
   Widget messageList() {
-    return Flexible(
-      child: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection('Chatroom/room1/messages')
-              // .collection('Chatroom')
-              // .doc('room1')
-              // .collection('messages')
-              .orderBy('time', descending: true)
-              .snapshots(),
-          // FirebaseFirestore.instance.collection('Chatroom/room1/messages').orderBy('timestamp', descending: true).snapshots(),
-          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasData){
-              listMessage
-            }
-            documents = snapshot.data!.docs;
-            return Expanded(
-              child: ListView.builder(
-                reverse: true,
-                itemCount: snapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  print(snapshot.data!.docs.length,);
-                  FirebaseFirestore.instance
-                      .collection('Chatroom/room1/messages')
-                      .where('sender', isEqualTo: '100')
-                      .get()
-                      .then((value) {
-                    print(value.docs.single.data().toString());
-                  });
-                  // print(documents.single.get('writer'));
-                  // print(documents.single.get('reader'));
-                  return _chats[index];
-                },
-              ),
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('Chatroom/room1/messages')
+            .orderBy('time')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          }
+          final documents = snapshot.data!.docs;
+          final _length = documents.length;
+          return Expanded(
+            child: ListView.builder(
+              reverse: true,
+              itemCount: _length,
+              itemBuilder: (context, index){
+                // return Message(documents[(_length-1)-index].get('text').toString());
+                return Message(documents[(_length-1)-index].get('sender').toString(),documents[(_length-1)-index].get('text').toString(), documents[(_length-1)-index].get('time'));
+              }),
             );
-          }),
-    ),
+        }
+    );
   }
 }
 
